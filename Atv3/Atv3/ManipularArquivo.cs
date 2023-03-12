f1511cfa10d3df1d1035afc0965c64c262acc743
VitorHRV <62753727+VitorXt@users.noreply.github.com>
2023-03-12 20:02:14 -0300

    Add files via upload

---
+++ Atv3/Atv3/ManipularArquivo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atv3
+{
+    public class ArquivoTexto
+    {
+        public string Conteudo;
+        public string EnderecoNomeArquivo;
+
+        public bool SalvarDadosArquivo()
+        {
+            try
+            {
+                File.WriteAllText(EnderecoNomeArquivo, Conteudo);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public string PesquisarConteudoArquivo(string endereco)
+        {
+            try
+            {
+                return File.ReadAllText(endereco);
+            }
+            catch
+            {
+                return "";
+            }
+        }
+    }
+}
--- Atv3/Atv3/Program.cs
+++ Atv3/Atv3/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using Atv3;
+
+ArquivoTexto arquivo = new ArquivoTexto();
+
+arquivo.Conteudo = "Exemplo de conteúdo a ser salvo em arquivo.";
+arquivo.EnderecoNomeArquivo = @"C:\temp\arquivo.txt";
+
+if (arquivo.SalvarDadosArquivo())
+{
+    Console.WriteLine("Conteúdo salvo com sucesso!");
+}
+else
+{
+    Console.WriteLine("Falha ao salvar conteúdo.");
+}
+
+string conteudoArquivo = arquivo.PesquisarConteudoArquivo(arquivo.EnderecoNomeArquivo);
+if (conteudoArquivo != "")
+{
+    Console.WriteLine("Conteúdo do arquivo:");
+    Console.WriteLine(conteudoArquivo);
+}
+else
+{
+    Console.WriteLine("Falha ao buscar conteúdo.");
+}
