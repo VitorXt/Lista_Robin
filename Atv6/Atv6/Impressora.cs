410c10004bed922fc3cd5ce493c455f858265fcb
VitorHRV <62753727+VitorXt@users.noreply.github.com>
2023-04-23 19:30:37 -0300

    Add files via upload

---
+++ Atv6/Atv6/Impressora.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atv6
+{
+    public class Impressora
+    {
+        public int qtdeCopia = 1;
+        private readonly Documento _documento;
+
+        public Impressora(Documento documento)
+        {
+            _documento = documento;
+        }
+        public void Imprimir()
+        {
+            for (int i = 0; i < qtdeCopia; i++)
+            {
+                Console.WriteLine(_documento.texto);
+            }
+        }
+    }
+}
---
+++ Atv6/Atv6/Imprimir.cs
@@ -0,0 +1,25 @@
+﻿using Atv6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atv6
+{
+    public class Imprimir
+    {
+        Documento documento = new Documento();
+        public void EnviarImpressao()
+        {
+            Impressora imp = new Impressora(documento);
+            imp.Imprimir();
+        }
+
+        public void SolicitarTexto()
+        {
+            Console.WriteLine("Digite aqui o texto que deseja imprimir:");
+            documento.texto = Console.ReadLine();
+        }
+    }
+}
